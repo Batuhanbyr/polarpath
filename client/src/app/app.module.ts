431818bf710280320a0a07ff5e004b5811d9cb8b
Batuhanbyr <bbayir686@gmail.com>
2024-05-28 17:09:46 +0300

    section8

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { PanelModule } from 'primeng/panel';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     InputTextModule,
-    PanelModule
+    PanelModule,
+    MenubarModule,
+    ButtonModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
